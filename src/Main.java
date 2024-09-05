b8122be43d5f901d271f8c8bb07879b67d19a811
Thabit Ibrahim <thabit252@gmail.com>
2024-09-05 14:47:15 +0800

    v1, backend module

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,36 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Create an instance of FileHandler
+        // Create instances of FileHandler and Project
         FileHandler fileHandler = new FileHandler();
+        Project project = new Project("ProjectA");
 
-        // Read transactions from the file in the 'data' directory
-        List<Transaction> transactions = fileHandler.readTransactions("transactions.txt");
+        // Add properties to the project
+        Property property1 = new Property("1500 sq ft", 300000, "3 bedrooms, pool", "ProjectA", "123 Street");
+        Property property2 = new Property("1600 sq ft", 320000, "4 bedrooms, gym", "ProjectA", "456 Avenue");
+        Property property3 = new Property("1200 sq ft", 250000, "2 bedrooms, garden", "ProjectA", "789 Boulevard");
 
-        // Display all transactions
-        System.out.println("Historical Transactions:");
+        project.addProperty(property1);
+        project.addProperty(property2);
+        project.addProperty(property3);
+
+        // Display all properties
+        System.out.println("All Properties:");
+        project.displayProperties();
+
+        // Search for properties with size "1500 sq ft" and price range 250000 to 350000
+        List<Property> searchResults = project.searchProperties("1500 sq ft", 250000, 350000, null);
+        System.out.println("\nSearch Results:");
+        for (Property p : searchResults) {
+            p.displayDetails();
+        }
+
+        // Load historical transactions from a file
+        project.loadHistoricalTransactions("transactions.txt");
+
+        // Display historical transactions (last 5 transactions)
+        System.out.println("\nHistorical Transactions:");
+        List<Transaction> transactions = project.getHistoricalTransactions();
         for (Transaction transaction : transactions) {
             transaction.displayTransactionDetails();
         }
