6568b3c525e18d0a8b5df3900676aa4d41f4060d
Thabit Ibrahim <thabit252@gmail.com>
2024-09-13 02:13:01 +0800

    Implement DropdownAutoSuggest

--- src/main/java/util/DropdownAutoSuggest.java
+++ src/main/java/util/DropdownAutoSuggest.java
@@ -1,5 +1,19 @@
 package main.java.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DropdownAutoSuggest {
-  
+    private List<String> projectNames;
+
+    public DropdownAutoSuggest(List<String> projectNames) {
+        this.projectNames = projectNames;
+    }
+
+    public List<String> getSuggestions(String input) {
+        return projectNames.stream()
+            .filter(name -> name.toLowerCase().startsWith(input.toLowerCase()))
+            .collect(Collectors.toList());
+    }
 }
--- src/main/java/view/ProjectView.java
+++ src/main/java/view/ProjectView.java
@@ -1,5 +1,31 @@
 package main.java.view;
 
+import java.util.List;
+import main.java.model.Project;
+
 public class ProjectView {
-  
+
+    public void displayProjects(List<Project> projects) {
+        if (projects.isEmpty()) {
+            System.out.println("No projects available.");
+        } else {
+            System.out.println("Available Projects:");
+            for (Project project : projects) {
+                System.out.println("Project Name: " + project.getProjectName());
+                // Additional project details can be added here
+            }
+        }
+    }
+
+    public void displayProjectDetails(Project project) {
+        System.out.println("Project Name: " + project.getProjectName());
+        System.out.println("Properties:");
+        project.getProperties().forEach(property -> {
+            System.out.println("Address: " + property.getAddress());
+            System.out.println("Size: " + property.getSize());
+            System.out.println("Price: " + property.getPrice());
+            System.out.println("Facilities: " + property.getFacilities());
+            System.out.println("-----");
+        });
+    }
 }
